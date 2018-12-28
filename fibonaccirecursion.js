b02803df7ae1ad888a544d94f33d0f192337a55b
nguyenduy7474 <nguyenduy7474@gmail.com>
2018-12-28 22:01:03 +0700

    Test xbot

---
+++ fibonaccirecursion.js
@@ -0,0 +1,8 @@
+function Fibonacci(n){
+  if(n<=2){
+    return 1
+  }
+  return Fibonacci(n - 1) + Fibonacci(n - 2)
+}
+
+console.log(Fibonacci(5))
---
+++ sumintergers.js
@@ -0,0 +1,8 @@
+let arr = [1,8,4,10]
+let sum = 0
+
+for(var i=0; i<arr.length; i++){
+	sum += arr[i]
+}
+
+console.log(sum)
